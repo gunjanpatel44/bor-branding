2af5214127227d18d179d8ff67a6e063ce33cb3a
Gunjan Bhingaradiya <gunjanbhingaradiya@gmail.com>
2025-10-02 22:28:31 +0530

    fix: remove api folder

--- app/api/review/review.route.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import * as reviewController from './review.controller'
-
-const router = express.Router()
-
-router.post('/', reviewController.addReview)
-router.get('/', reviewController.getReviews)
-
-export default router
