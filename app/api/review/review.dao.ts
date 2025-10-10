4ce49c448c441995c41092b3a2d711d7c81c2beb
Gunjan Bhingaradiya <gunjanbhingaradiya@gmail.com>
2025-10-11 00:39:33 +0530

    refactor: remove review API components and related files

--- app/api/review/review.dao.ts
+++
@@ -1,6 +0,0 @@
-import Review from './review.model'
-
-// export const createReview = (data) => Review.create(data)
-// export const getReviewsByProduct = (productId) => Review.find({ productId })
-// export const getReviewsByCategory = (category) => Review.find({ category })
-// export const getAllReviews = () => Review.find()
